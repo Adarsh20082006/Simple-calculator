2d230439fde5d8cdc25981571c5caf3e9cc64f9e
Adarsh M M <adarshakashmm@gmail.com>
2024-04-30 22:38:53 +0530

    errors catching

--- Calci.js
+++ Calci.js
@@ -1,6 +1,8 @@
+// document.querySelector(".display").style.visibility="hidden";
+//  document.querySelector(".display-2").style.visibility="hidden";
 function data(a){ 
     document.querySelector(".display").value+=a;
-    document.querySelector(".display-2").style.visibility="visible";
+   //  document.querySelector(".display-2").style.visibility="visible";
     let val=eval(document.querySelector(".display").value);
     val=val.toFixed(2);
     document.querySelector(".display-2").value=val;
@@ -13,6 +15,42 @@ function data(a){
        document.querySelector(".display").id="";
     }
    }
+   function on(){
+   if(document.querySelector(".display").style.visibility=="visible"){
+      document.querySelector(".display").style.visibility="hidden";
+      document.querySelector(".display-2").style.visibility="hidden";
+   }
+   else{
+      document.querySelector(".display").style.visibility="visible";
+      document.querySelector(".display-2").style.visibility="visible";
+      document.querySelector(".display-2").value="";
+      document.querySelector(".display").value="";
+      setTimeout(welcome,500)
+   
+   } }
+   if(((((document.querySelector(".display").value=="NaN" || document.querySelector(".display-2").value==NaN) || document.querySelector(".display").value==undefined) || document.querySelector(".display-2").value==undefined) || document.querySelector(".display").value==Infinity) || document.querySelector(".display-2").value==Infinity){
+      document.querySelector(".display").value="Error🥴";
+      document.querySelector(".display-2").value="";
+   }
+   function welcome(){
+    
+    setTimeout(()=>{document.querySelector(".display").value="W"},450)
+    setTimeout(()=>{document.querySelector(".display").value+="e"},500)
+    setTimeout(()=>{document.querySelector(".display").value+="l"},550)
+    setTimeout(()=>{document.querySelector(".display").value+="c"},600)
+    setTimeout(()=>{document.querySelector(".display").value+="o"},650)
+    setTimeout(()=>{document.querySelector(".display").value+="m"},700)
+    setTimeout(()=>{document.querySelector(".display").value+="e"},750)
+    setTimeout(()=>{document.querySelector(".display").value+=" "},800)
+    setTimeout(()=>{document.querySelector(".display").value+="t"},850)
+     setTimeout(()=>{document.querySelector(".display").value+="h"},900)
+     setTimeout(()=>{document.querySelector(".display").value+="e"},950)
+     setTimeout(()=>{document.querySelector(".display").value+="r"},1000)
+     setTimeout(()=>{document.querySelector(".display").value+="e"},1050)
+     setTimeout(()=>{document.querySelector(".display").value+="!"},1100)
+     setTimeout(()=>{document.querySelector(".display").value=""},1500)
+   }
+   
    function data1(){
 document.querySelector(".display").value=eval(document.querySelector(".display").value);
 document.querySelector(".display-2").style.visibility="hidden";
@@ -112,36 +150,39 @@ function tan(x){
       x=document.querySelector(".display").value;
       document.querySelector(".display").value="l0g("+x+")";
       document.querySelector(".display-2").value=Math.log10(x).toFixed(4);
+      open1();
       document.querySelector(".eql").addEventListener('click',()=>{
          document.querySelector(".display").value=Math.lo1g10(x).toFixed(4);
       document.querySelector(".display-2").value="";
-      open1();
+   
    })}
    function ln(){
       x=document.querySelector(".display").value;
       document.querySelector(".display").value="ln("+x+")";
       document.querySelector(".display-2").value=Math.log(x).toFixed(4);
+      open1();
       document.querySelector(".eql").addEventListener('click',()=>{
          document.querySelector(".display").value=Math.log(x).toFixed(4);
       document.querySelector(".display-2").value="";
       })
-      open1();
+    
    }
    function expo(){
       x=document.querySelector(".display").value;
       document.querySelector(".display").value="e^("+x+")";
       document.querySelector(".display-2").value=Math.exp(x).toFixed(4);
+      open1();
       document.querySelector(".eql").addEventListener('click',()=>{
          document.querySelector(".display").value=Math.exp(x).toFixed(4);
       document.querySelector(".display-2").value="";
-      open1();
    })}
    function x3(){
       x=document.querySelector(".display").value;
       document.querySelector(".display").value=x+"^3";
       document.querySelector(".display-2").value=x**3;
+      open1();
       document.querySelector(".eql").addEventListener('click',()=>{
          document.querySelector(".display").value=x**3;
       document.querySelector(".display-2").value="";
-      open1();
+  
    })}
